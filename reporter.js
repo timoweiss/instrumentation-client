0fade2bababcac4228abfea057e417d7ab2144f6
Timo Weiß <info@timo-weiss.com>
2016-05-04 11:27:00 +0200

    update reporter for new collector data-structure

---
+++ reporter.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const http = require('http');
+
+const options = {};
+let collector = {};
+
+module.exports = function reporter(config, _collector) {
+    options.reportingEndpoint = '/report/' + config.application_id + '/' + config.service_name
+
+    collector = _collector;
+
+    setInterval(report, 10000);
+
+    return {
+        report
+    }
+};
+
+function report() {
+
+    const data = collector.flush();
+
+    if (!data.inRequest.length && !data.outRequest.length && !data.outResponse.length && !data.inResponse.length) {
+        console.log('noting to report, returning');
+        return;
+    }
+
+    const postData = JSON.stringify(data);
+
+    const request = http.request({
+        hostname: 'localhost',
+        port: 2020,
+        method: 'POST',
+        path: options.reportingEndpoint
+    }, function (res) {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.')
+        })
+    });
+
+    request.on('error', (e) => {
+        console.log(`problem with request: ${e.message}`);
+    });
+
+    // write data to request body
+    request.write(postData);
+    request.end();
+}
