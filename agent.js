3415c7df26baee8562e974b3cae66afad669cd4b
Timo Weiß <info@timo-weiss.com>
2016-05-06 00:59:15 +0200

    init agent

---
+++ agent.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+function Agent(config) {
+    this.application_id = config.application_id;
+    this.service_name = config.service_name;
+    this.orga_id = config.orga_id;
+};
+
+Agent.prototype.getServiceInformation = function _getServiceInformation() {
+    return {
+        application_id: this.application_id,
+        service_name: this.service_name,
+        orga_id: this.orga_id,
+        process_info: {
+            pid: process.pid
+        }
+    }
+};
+
+module.exports = Agent;
