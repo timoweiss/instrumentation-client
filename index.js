8b1a666a00e3438931ba4c7d927a8a04147c362a
Timo Weiß <info@timo-weiss.com>
2016-05-11 00:02:18 +0200

    move namespacefns, update options

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 
 const cls = require('continuation-local-storage');
+const uuid = require('node-uuid');
 
 const collector = require('./lib/collector');
+const reporter = require('./lib/reporter');
 const Agent = require('./lib/agent');
 
 
@@ -11,37 +13,37 @@ module.exports.start = function start(options) {
     console.log('starting instrumentations');
 
     const agent = new Agent(options);
-
+    const namespace = cls.createNamespace('TODO');
 
     options.collector = collector;
-    options.agent = collector;
-    options.namespaceFns = namespaceFns;
-    options.namespace = cls.createNamespace('TODO');
+    options.agent = agent;
+    options.reporter = reporter;
+
+    options.namespaceFns = {
+        setTraceId: function(tid) {
+            return namespace.set('tid', tid);
+        },
+        getTraceId: function() {
+            return namespace.get('tid');
+        },
+        setRequestId: function(tid) {
+            return namespace.set('rid', tid);
+        },
+        getRequestId: function() {
+            return namespace.get('rid');
+        },
+        generateTraceId: function makeid() {
+            return uuid.v4();
+        },
+        generateRequestId: function makeid() {
+            return uuid.v4();
+        },
+        bind: function(fn) {
+            return namespace.bind(fn)
+        }
+    };
+    options.namespace = namespace;
 
 
     require('./senecaShim').start(options);
-};
-
-const namespaceFns = {
-    setTraceId: function(tid) {
-        return namespace.set('tid', tid);
-    },
-    getTraceId: function() {
-        return namespace.get('tid');
-    },
-    setRequestId: function(tid) {
-        return namespace.set('rid', tid);
-    },
-    getRequestId: function() {
-        return namespace.get('rid');
-    },
-    generateTraceId: function makeid() {
-        return uuid.v4();
-    },
-    generateRequestId: function makeid() {
-        return uuid.v4();
-    },
-    bind: function(fn) {
-        return namespace.bind(fn)
-    }
 };
