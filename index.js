7cbe08604254ae99c754cd09c3a318828a964bc3
Timo Weiß <info@timo-weiss.com>
2016-05-10 22:29:02 +0200

    add collector and agent to options

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict';
 
 
-const collector = require('./collector')();
+const collector = require('./collector');
 const Agent = require('./agent');
 
 
 module.exports.start = function start(options) {
     console.log('starting instrumentations');
+
+    const agent = new Agent(options);
+
+    options.collector = collector;
+    options.agent = collector;
+
 
     require('./senecaShim').start(options);
 };
